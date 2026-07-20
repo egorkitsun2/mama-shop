78c4d8cdf8d4bfcc57f458447a7f9bc08c1185f3
Egor <kitsunegor@gmail.com>
2026-07-20 18:00:32 +0500

    оптимизирование автопуша на гитфаб

--- push_data.sh
+++ push_data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd /home/sakura/mama-shop
-git add products.json static/img
-# Конструкция || true спасает от падения скрипта, если попытались пушить без реальных изменений
-git commit -m "Автообновление прайса и фото бд" || true
-git push 
+
+# Добавляем только статику для сайта
+git add products.json static/img/*
+
+# Сохраняем слепок
+git commit -m "Автообновление прайса и фото" || true
+
+# Железобетонное стягивание твоего кода перед отправкой
+git pull --rebase --autostash
+
+# Пуляем в облако
+git push
